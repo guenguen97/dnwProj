4a78251acd167d8b2a2765fdf2226447aa23f511
유ìguenguen97 <guenguen9797@gmail.com>
2024-01-07 00:23:52 +0900

    html 연결

---
+++ src/main/java/com/doNotWorry/Main.java
@@ -0,0 +1,18 @@
+package com.doNotWorry;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class Main {
+
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+
+
+}
