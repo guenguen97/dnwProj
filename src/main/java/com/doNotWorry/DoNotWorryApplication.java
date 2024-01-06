9089e69f2abc7ea2ebe980e5315fca282cbf085c
유ìguenguen97 <guenguen9797@gmail.com>
2024-01-06 23:39:58 +0900

    이게 진짜 첫 ì 프로젝ã트 시작

---
+++ src/main/java/com/doNotWorry/DoNotWorryApplication.java
@@ -0,0 +1,13 @@
+package com.doNotWorry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoNotWorryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoNotWorryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doNotWorry/DoNotWorryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doNotWorry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoNotWorryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
