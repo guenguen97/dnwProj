820b8ffbaaef274d2b3d1cca63ccfda8eff30890
guenguen97 <guenguen97@gmail.com>
2024-01-15 17:29:19 +0900

    api키 보안으로 서버에서 키 주는 방식으로 대체

--- src/main/java/com/doNotWorry/DoNotWorryApplication.java
+++ src/main/java/com/doNotWorry/DoNotWorryApplication.java
@@ -2,8 +2,11 @@ package com.doNotWorry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secret.yml")
+
 public class DoNotWorryApplication {
 
 	public static void main(String[] args) {
