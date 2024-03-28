c9afbc078ffaef6f934e19ae8445429c241f8a4d
guenguen97 <guenguen97@gmail.com>
2024-03-28 16:48:37 +0900

    저장

--- src/main/resources/static/js/getCurrentPosition.js
+++ src/main/resources/static/js/getCurrentPosition.js
@@ -75,7 +75,7 @@ $(window).on("load", function() {
          * navigator.geolocation 은 Chrome 50 버젼 이후로 HTTP 환경에서 사용이 Deprecate 되어 HTTPS 환경에서만 사용 가능 합니다.
          * http://localhost 에서는 사용이 가능하며, 테스트 목적으로, Chrome 의 바로가기를 만들어서 아래와 같이 설정하면 접속은 가능합니다.
          * chrome.exe --unsafely-treat-insecure-origin-as-secure="http://example.com"
-         */
+         *
         navigator.geolocation.getCurrentPosition(onSuccessGeolocation, onErrorGeolocation);
     } else {
         var center = map.getCenter();
@@ -309,6 +309,7 @@ for (var key in MARKER_SPRITE_POSITION) {
                 infoWindow.open(map, marker);
                  function displayComments(list) {
                                          const commentsContainer = document.getElementById('menuContainer');
+                                         commentsContainer.style.display = "inline-block";
                                          commentsContainer.innerHTML = ''; //클릭될떄마다 메뉴는 초기화
                                         commentsContainer.innerHTML += `
                                             <div class="top-container" style="display:flex;">
@@ -316,7 +317,7 @@ for (var key in MARKER_SPRITE_POSITION) {
                                                     ${marker.storeName}
                                                 </div>
                                                 <div class="likeButton" onclick="likeOrNot(${marker.id});">
-                                                    저장
+                                                    즐겨찾기
                                                 </div>
                                             </div>
 
