92b1964f24cbee2f67f021c4bf249e13392d96b7
guenguen97 <guenguen97@gmail.com>
2024-04-11 17:14:51 +0900

    디비 음식점 중복 저장 오류 가 있는거 같은데 랩탑에서도 있는지 확인 해야함

--- src/main/java/com/doNotWorry/menu/MenuController.java
+++ src/main/java/com/doNotWorry/menu/MenuController.java
@@ -21,7 +21,10 @@ public class MenuController {
     @ResponseBody
     public List<String> getMenu(@PathVariable(name = "id") final Integer id){
         System.out.println("메뉴 불러오기 api 실행");
+        System.out.println("음식점 id는"+id);
         List<String> menuList= menuService.findMenuListById(id);
+        System.out.println("메뉴 리스트의 크기는"+menuList.size());
+
         for (int i = 0; i < menuList.size(); i++) {
             System.out.println(menuList.get(i));
         }
